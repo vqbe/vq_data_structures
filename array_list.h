a097e1b912c95a2a14c6658d799fe816f052af0d
Irinel Sarbu <razvanirinel.sarbu@gmail.com>
2022-09-06 15:29:11 +0200

    Array list interface implementation

--- array_list.h
+++ array_list.h
@@ -17,21 +17,19 @@ extern "C" {
 #define VQ_DEFAULT_ARRAY_LIST_SIZE 8
 #endif
 
-typedef struct array_list_internal array_list_internal_t;
+  typedef struct array_list array_list_t;
 
-typedef struct array_list_t {
-  array_list_internal_t *m_fields;
+  VQ_DEF_TYPE array_list_t* array_list_new(size_t element_size);
+  VQ_DEF_TYPE void array_list_destroy(array_list_t* list);
 
-  void (*add)(struct array_list_t *list, void *element);
-  void (*contains)(struct array_list_t *list, void *element);
-  void (*remove)(struct array_list_t *list, void *element);
-  void (*get_at)(struct array_list_t *list, unsigned int index);
-  void (*remove_at)(struct array_list_t *list, unsigned int index);
-  void (*clear)(struct array_list_t *list);
-} array_list_t;
+  VQ_DEF_TYPE size_t array_list_get_size(array_list_t* list);
 
-VQ_DEF_TYPE array_list_t *new_array_list(size_t element_size);
-VQ_DEF_TYPE void destroy_array_list(array_list_t *list);
+  VQ_DEF_TYPE void array_list_add(array_list_t* list, void* element);
+  VQ_DEF_TYPE void array_list_contains(array_list_t* list, void* element);
+  VQ_DEF_TYPE void array_list_remove(array_list_t* list, void* element);
+  VQ_DEF_TYPE void* array_list_get_at(array_list_t* list, unsigned int index);
+  VQ_DEF_TYPE void array_list_remove_at(array_list_t* list, unsigned int index);
+  VQ_DEF_TYPE void array_list_clear(array_list_t* list);
 
 #ifdef __cplusplus
 }
@@ -39,52 +37,59 @@ VQ_DEF_TYPE void destroy_array_list(array_list_t *list);
 
 #ifdef VQ_ARRAY_LIST_IMPLEMENTATION
 
-typedef struct array_list_internal {
+
+#include <stdlib.h>
+
+typedef struct array_list {
   size_t element_size;
   size_t count;
-  size_t size;
-  void **data;
-} array_list_internal_t;
+  size_t capacity;
+  void** data;
+} array_list_t;
 
-static void add(array_list_t *list, void *element) {}
-static void contains(array_list_t *list, void *element) {}
-static void remove(array_list_t *list, void *element) {}
-static void get_at(array_list_t *list, unsigned int index) {}
-static void remove_at(array_list_t *list, unsigned int index) {}
-static void clear(array_list_t *list) {}
+static void resize(array_list_t* list) {
+  list->capacity = list->capacity << 1;
+  list->data = (void**)realloc(list->data, list->element_size * list->capacity);
+}
 
-array_list_t *new_array_list(size_t element_size) {
-  array_list_internal_t *m_fields =
-      (array_list_internal_t *)malloc(sizeof(array_list_internal_t));
-  m_fields->element_size = element_size;
-  m_fields->count = 0;
-  m_fields->size = VQ_DEFAULT_ARRAY_LIST_SIZE;
-  m_fields->data = (void **)malloc(sizeof(void *) * VQ_DEFAULT_ARRAY_LIST_SIZE);
-
-  if (m_fields->data == nullptr)
-    return nullptr;
-
-  array_list_t *new_array_list = (array_list_t *)malloc(sizeof(array_list_t));
-  new_array_list->m_fields = m_fields;
-
-  new_array_list->add = add;
-  new_array_list->contains = contains;
-  new_array_list->remove = remove;
-  new_array_list->get_at = get_at;
-  new_array_list->remove_at = remove_at;
-  new_array_list->clear = clear;
-
+array_list_t* array_list_new(size_t element_size) {
+  array_list_t* new_array_list = (array_list_t*)malloc(sizeof(array_list_t));
+  new_array_list->element_size = element_size;
+  new_array_list->count = 0;
+  new_array_list->capacity = VQ_DEFAULT_ARRAY_LIST_SIZE;
+  new_array_list->data = (void**)malloc(sizeof(void*) * VQ_DEFAULT_ARRAY_LIST_SIZE);
   return new_array_list;
 }
 
-void destroy_array_list(array_list_t *list) {
-  for (unsigned int i = 0; i < list->m_fields->size; i++) {
-    free(list->m_fields->data[i]);
-  }
-  free(list->m_fields->data);
-  free(list->m_fields);
+void array_list_destroy(array_list_t* list) {
+  free(list->data);
   free(list);
 }
+
+size_t array_list_get_size(array_list_t* list) {
+  return list->count;
+}
+
+void array_list_add(array_list_t* list, void* element) {
+  if (list->count == list->capacity)
+    resize(list);
+
+  size_t index = list->count;
+  list->data[index] = element;
+  list->count++;
+}
+
+void array_list_contains(array_list_t* list, void* element) {}
+
+void array_list_remove(array_list_t* list, void* element) {}
+
+void* array_list_get_at(array_list_t* list, unsigned int index) {
+  return list->data[index];
+}
+
+void array_list_remove_at(array_list_t* list, unsigned int index) {}
+
+void array_list_clear(array_list_t* list) {}
 
 #endif
 
